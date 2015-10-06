3dcd265aab27f4ab4bd847d3717c8937d740fba1
NicHub <nicolas.jeanmonod@gmail.com>
2015-10-06 15:10:39 +0200

    article sites statiques

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -34,22 +34,8 @@ $(function() {
 $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
 
 
-function targetBlank() {
-  // remove subdomain of current site's url and setup regex
-  var internal = location.host.replace( "www.", "" );
-  internal = new RegExp( internal, "i" );
-  var a = document.getElementsByTagName( 'a' ); // then, grab every link on the page
-  for( var i = 0; i < a.length; i++ ) {
-    var href = a[ i ].host; // set the host of each link
-    if( !internal.test( href ) ) { // make sure the href doesn't contain current site's host
-          a[ i ].setAttribute( 'target', '_blank' ); // if it doesn't, set attributes
-      }
-  }
-};
-
 // Magnific-Popup options
 $(document).ready(function() {
-  targetBlank();
   $('.image-popup').magnificPopup({
     type: 'image',
     tLoading: 'Loading image #%curr%...',
