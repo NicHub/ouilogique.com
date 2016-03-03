d7ec52e9367bd617fdb02183a0b4f345e0e089b3
NicHub <nicolas.jeanmonod@gmail.com>
2016-03-03 16:37:56 +0100

    upgrade Jekyll + compress.html

--- _serve.sh
+++ _serve.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # bundle exec jekyll build --config _config.yml,_config_dev.yml
-bundle exec jekyll serve --config _config.yml,_config_dev.yml --safe
+bundle exec jekyll serve --config _config.yml,_config_dev.yml  --incremental
