c4e89289737be7b5937fe0d35ebb31ec24c24d8d
NicHub <nicolas.jeanmonod@gmail.com>
2023-04-20 13:31:14 +0200

    Fromat playlist as json

--- radios/radio_station_playlist.js
+++ radios/radio_station_playlist.js
@@ -1,9 +1,11 @@
-let radio_station_playlist = [
+let radio_station_playlist =
+[
     {
         "geo_name": "France",
         "station_name": "France Inter",
         "live_urls": [
-            "https://www.radiofrance.fr/franceinter/direct"],
+            "https://www.radiofrance.fr/franceinter/direct"
+        ],
         "stream_urls": [
             "https://direct.franceinter.fr/live/franceinter-midfi.mp3"
         ]
@@ -11,7 +13,9 @@ let radio_station_playlist = [
     {
         "geo_name": "France",
         "station_name": "Europe 1",
-        "live_urls": ["https://www.europe1.fr/direct-video"],
+        "live_urls": [
+            "https://www.europe1.fr/direct-video"
+        ],
         "stream_urls": [
             "https://stream.europe1.fr/europe1.aac",
             "https://stream.europe1.fr/europe1.mp3"
@@ -20,7 +24,9 @@ let radio_station_playlist = [
     {
         "geo_name": "France",
         "station_name": "Europe 2",
-        "live_urls": ["https://www.europe2.fr/webradios"],
+        "live_urls": [
+            "https://www.europe2.fr/webradios"
+        ],
         "stream_urls": [
             "https://europe2.lmn.fm/vr-wr2.aac",
             "https://europe2.lmn.fm/vr-wr2.mp3"
@@ -29,7 +35,9 @@ let radio_station_playlist = [
     {
         "geo_name": "France",
         "station_name": "France Culture",
-        "live_urls": ["https://www.radiofrance.fr/franceculture"],
+        "live_urls": [
+            "https://www.radiofrance.fr/franceculture"
+        ],
         "stream_urls": [
             "https://direct.franceculture.fr/live/franceculture-hifi.aac",
             "https://direct.franceculture.fr/live/franceculture-midfi.mp3"
@@ -38,13 +46,19 @@ let radio_station_playlist = [
     {
         "geo_name": "France",
         "station_name": "Radio 74",
-        "live_urls": ["https://www.radio.fr/s/radio74"],
-        "stream_urls": ["https://radio74.ice.infomaniak.ch/radio74.aac"]
+        "live_urls": [
+            "https://www.radio.fr/s/radio74"
+        ],
+        "stream_urls": [
+            "https://radio74.ice.infomaniak.ch/radio74.aac"
+        ]
     },
     {
         "geo_name": "France",
         "station_name": "France Musique",
-        "live_urls": ["https://www.radiofrance.fr/francemusique"],
+        "live_urls": [
+            "https://www.radiofrance.fr/francemusique"
+        ],
         "stream_urls": [
             "https://direct.francemusique.fr/live/francemusique-midfi.mp3",
             "https://direct.francemusique.fr/live/francemusique-lofi.mp3"
@@ -53,14 +67,22 @@ let radio_station_playlist = [
     {
         "geo_name": "Suisse romande",
         "station_name": "RTS — La 1ère",
-        "live_urls": ["https://www.rts.ch/audio-podcast/livepopup/la-1ere/"],
-        "stream_urls": ["https://stream.srg-ssr.ch/m/la-1ere/mp3_128"]
+        "live_urls": [
+            "https://www.rts.ch/audio-podcast/livepopup/la-1ere/"
+        ],
+        "stream_urls": [
+            "https://stream.srg-ssr.ch/m/la-1ere/mp3_128"
+        ]
     },
     {
         "geo_name": "Suisse romande",
         "station_name": "RTS — Espace 2",
-        "live_urls": ["https://www.rts.ch/audio-podcast/livepopup/espace-2/"],
-        "stream_urls": ["https://stream.srg-ssr.ch/m/espace-2/mp3_128"]
+        "live_urls": [
+            "https://www.rts.ch/audio-podcast/livepopup/espace-2/"
+        ],
+        "stream_urls": [
+            "https://stream.srg-ssr.ch/m/espace-2/mp3_128"
+        ]
     },
     {
         "geo_name": "Suisse romande",
@@ -69,7 +91,9 @@ let radio_station_playlist = [
             "https://www.rts.ch/audio-podcast/livepopup/couleur3/",
             "https://www.rts.ch/play/tv/direct/couleur-3?tvLiveId=8841634"
         ],
-        "stream_urls": ["https://stream.srg-ssr.ch/m/couleur3/mp3_128"]
+        "stream_urls": [
+            "https://stream.srg-ssr.ch/m/couleur3/mp3_128"
+        ]
     },
     {
         "geo_name": "Suisse romande",
@@ -77,36 +101,56 @@ let radio_station_playlist = [
         "live_urls": [
             "https://www.rts.ch/audio-podcast/livepopup/option-musique/"
         ],
-        "stream_urls": ["https://stream.srg-ssr.ch/m/option-musique/mp3_128"]
+        "stream_urls": [
+            "https://stream.srg-ssr.ch/m/option-musique/mp3_128"
+        ]
     },
     {
         "geo_name": "Suisse romande",
         "station_name": "SRG SSR — Radio Swiss Pop",
-        "live_urls": ["https://www.radioswisspop.ch/fr"],
-        "stream_urls": ["https://stream.srg-ssr.ch/m/rsp/mp3_128"]
+        "live_urls": [
+            "https://www.radioswisspop.ch/fr"
+        ],
+        "stream_urls": [
+            "https://stream.srg-ssr.ch/m/rsp/mp3_128"
+        ]
     },
     {
         "geo_name": "Suisse romande",
         "station_name": "SRG SSR — Radio Swiss Jazz",
-        "live_urls": ["https://www.radioswissjazz.ch/fr"],
-        "stream_urls": ["https://stream.srg-ssr.ch/m/rsj/mp3_128"]
+        "live_urls": [
+            "https://www.radioswissjazz.ch/fr"
+        ],
+        "stream_urls": [
+            "https://stream.srg-ssr.ch/m/rsj/mp3_128"
+        ]
     },
     {
         "geo_name": "Suisse romande",
         "station_name": "SRG SSR — Radio Swiss Classic",
-        "live_urls": ["https://www.radioswissclassic.ch/fr"],
-        "stream_urls": ["https://stream.srg-ssr.ch/m/rsc_fr/mp3_128"]
+        "live_urls": [
+            "https://www.radioswissclassic.ch/fr"
+        ],
+        "stream_urls": [
+            "https://stream.srg-ssr.ch/m/rsc_fr/mp3_128"
+        ]
     },
     {
         "geo_name": "Suisse romande",
         "station_name": "Radio Lac — Genève",
-        "live_urls": ["https://www.radiolac.ch/direct/player/"],
-        "stream_urls": ["https://radiolac.ice.infomaniak.ch/radiolac-high.mp3"]
+        "live_urls": [
+            "https://www.radiolac.ch/direct/player/"
+        ],
+        "stream_urls": [
+            "https://radiolac.ice.infomaniak.ch/radiolac-high.mp3"
+        ]
     },
     {
         "geo_name": "Suisse romande",
         "station_name": "Radio Chablais",
-        "live_urls": ["https://www.radiochablais.ch/player/"],
+        "live_urls": [
+            "https://www.radiochablais.ch/player/"
+        ],
         "stream_urls": [
             "https://radiochablais.ice.infomaniak.ch/radiochablais-high.mp3"
         ]
@@ -114,7 +158,9 @@ let radio_station_playlist = [
     {
         "geo_name": "Suisse romande",
         "station_name": "LFM — Lausanne FM",
-        "live_urls": ["https://www.lfm.ch/player/popup/index.html"],
+        "live_urls": [
+            "https://www.lfm.ch/player/popup/index.html"
+        ],
         "stream_urls": [
             "https://lausannefm.ice.infomaniak.ch/lausannefm-high.mp3"
         ]
@@ -122,7 +168,11 @@ let radio_station_playlist = [
     {
         "geo_name": "USA — NY",
         "station_name": "Boss Radio 66",
-        "live_urls": ["https://wfmuichiban.blogspot.com"],
-        "stream_urls": ["https://rfcmedia.streamguys1.com/bossradio66"]
+        "live_urls": [
+            "https://wfmuichiban.blogspot.com"
+        ],
+        "stream_urls": [
+            "https://rfcmedia.streamguys1.com/bossradio66"
+        ]
     }
 ];
