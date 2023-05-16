1ed1ba8134d1f58b8fa6f4be30d2de6a4944bef1
NicHub <nicolas.jeanmonod@gmail.com>
2023-05-16 17:17:05 +0200

    png icon

---
+++ radios/svg2png.sh
@@ -0,0 +1,33 @@
+###
+#
+##
+
+shopt -s expand_aliases
+alias inkscape='/Applications/Inkscape.app/Contents/MacOS/Inkscape'
+
+FILE_NAMES=(
+    favicon
+)
+for file_name in "${FILE_NAMES[@]}"; do
+
+    inkscape                      \
+        --export-area-page        \
+        --export-width 512        \
+        --export-height 512       \
+        "$file_name.svg"          \
+        -o "$file_name.png"
+
+    pngquant                      \
+        --strip                   \
+        --speed 1                 \
+        --verbose                 \
+        --force                   \
+        --skip-if-larger          \
+        --quality 0-2             \
+        3                         \
+        --output "$file_name.png" \
+        "$file_name.png"
+
+    open -a ImageOptim.app "$file_name.png"
+
+done
